8ef7e5a2c563dca9c6c26ce2eb2a2edbee4d2aa9
Turino <turino@outlook.com>
2018-02-26 22:58:11 -0300

    Add readme

--- src/TMDBApp.Core/GlobalSetting.cs
+++ src/TMDBApp.Core/GlobalSetting.cs
@@ -4,6 +4,6 @@ namespace TMDBApp
     public class GlobalSetting
     {
         public static string BackendUrl = "https://api.themoviedb.org/3/";
-        public static string ApiKey = "1f54bd990f1cdfb230adb312546d765d";
+        public static string ApiKey = "";
     }
 }
